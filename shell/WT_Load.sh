ac9cfb245e185a523f2de63955f94d39896bf9d2
MK <slfs009@163.com>
2016-08-18 14:11:11 +0800

    add WT_Load.sh

---
+++ shell/WT_Load.sh
@@ -0,0 +1,13 @@
+
+load_file=/mnt/hdd/data/wiki_load.txt.gz
+dirname=./experiment/wiki_wt
+keys_file=~/Documents/data/wiki_keys
+cd ../
+cmake ./CMakeLists.txt
+make
+cd shell
+echo "####Now, running wiredtiger benchmark"
+
+export MYSQL_PASSWD=$1
+rm -rf $dirname/*
+zcat $load_file | ../build/Terark_Engine_Test wiredtiger -load_or_run=load -keys_data_path=$keys_file --db=$dirname --use_lsm=0 -load_data_path=/dev/stdin
