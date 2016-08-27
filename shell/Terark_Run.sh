f139d20c9bf73a9d1c5bc799b7d69cc139113a6f
mk <slfs009@163.com>
2016-08-27 16:49:04 +0800

    update

--- shell/Terark_Run.sh
+++ shell/Terark_Run.sh
@@ -1,7 +1,6 @@
-
-keys_file=~/Documents/data/wiki_keys
+keys_file=~/Documents/data/wiki_keys_terark
 insert_file=/mnt/hdd/data/xab
-dirname=./experiment/new_wiki/
+dirname=./experiment/terark_wiki/
 cd ../
 cmake CMakeLists.txt
 make
@@ -9,12 +8,12 @@ cd shell
 export TMPDIR=$dirname
 export DictZipBlobStore_zipThreads=8
 echo $TMPDIR
-cp ../Schema/dbmeta_wikipedia_index_int.json $dirname/dbmeta.json
+ln -s ../Schema/dbmeta_wikipedia_index_int.json $dirname/dbmeta.json
 echo "####Now, running terarkdb benchmark"
 echo 3 > /proc/sys/vm/drop_caches
 date
 export TerarkDB_WrSegCacheSizeMB=500
 export MYSQL_PASSWD=$1
-../build/Terark_Engine_Test terarkdb -read_percent=100 -insert_percent=0 -load_or_run=run -insert_data_path=$insert_file --sync_index=1 --db=$dirname --threads=8 -keys_data_path=$keys_file
+../build/Terark_Engine_Test terarkdb -read_percent=90 -insert_percent=5 -load_or_run=run -insert_data_path=$insert_file --sync_index=1 --db=$dirname --threads=8 -keys_data_path=$keys_file
 date
 echo "####terarkdb benchmark finish"
