05312a43bac7fa08987e2ac1a377f26bf2bbbf36
mk <slfs009@163.com>
2016-09-01 09:54:33 +0800

    add DictZipBlobStore_zipThreads and TerarkDB_CompressionThreadsNum

--- shell/Terark_Run.sh
+++ shell/Terark_Run.sh
@@ -8,10 +8,12 @@ cd shell
 export TMPDIR=$dirname
 export DictZipBlobStore_zipThreads=8
 echo $TMPDIR
-ln -s ../Schema/dbmeta_wikipedia_index_int.json $dirname/dbmeta.json
+ln ../Schema/dbmeta_wikipedia_index_int.json $dirname/dbmeta.json
 echo "####Now, running terarkdb benchmark"
 echo 3 > /proc/sys/vm/drop_caches
 date
+export DictZipBlobStore_zipThreads=2
+export TerarkDB_CompressionThreadsNum=1
 export TerarkDB_WrSegCacheSizeMB=500
 export MYSQL_PASSWD=$1
 ../build/Terark_Engine_Test terarkdb -read_percent=90 -insert_percent=5 -load_or_run=run -insert_data_path=$insert_file --sync_index=1 --db=$dirname --threads=8 -keys_data_path=$keys_file
