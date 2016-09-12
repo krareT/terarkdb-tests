55478693b552ea8da778d33e7fa5f106521d6c5d
MK <slfs009@163.com>
2016-09-12 11:01:51 +0800

    Add Terark_Rocksdb_Tmp_dir

--- src/engine/src/TerarkRocksDbBenchmark.h
+++ src/engine/src/TerarkRocksDbBenchmark.h
@@ -12,9 +12,13 @@ public:
     TerarkRocksDbBenchmark(Setting &set) : RocksDbBenchmark(set) {
         std::cout <<"TR Benchmark" << std::endl;
         rocksdb::TerarkZipTableOptions opt;
-        if ( opt.localTempDir != "/tmp")
-            opt.localTempDir = "/tmp";
-        rocksdb::TableFactory* factory = NewTerarkZipTableFactory(opt);
+        char *tmp_dir = getenv("TerRocksdb_Tmpdir");
+	if ( tmp_dir == NULL || strlen(tmp_dir) == 0){
+		throw std::invalid_argument("U must set the tmp_dir!\n");
+	}
+        opt.localTempDir = tmp_dir;
+       	printf("local temp dir:%s\n",tmp_dir);
+       	rocksdb::TableFactory* factory = NewTerarkZipTableFactory(opt);
         options.table_factory.reset(factory);
     }
 
